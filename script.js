e60efafe172e632e8fec6a90fb2f7304d21afcdb
Ehrlacker <101287793+Ehrlacker@users.noreply.github.com>
2022-08-02 18:19:14 -0400

    Magic 8 ball game

--- script.js
+++ script.js
@@ -1,35 +1,49 @@
 "use strict";
 
-let randomNumber = Math.floor(Math.random() * 8);
+//Call .question
+//.answer
+//#clickMe
 
+const questions = document.querySelector(".question");
+const answers = document.querySelector(".answer");
+const clickButton = document.querySelector("#clickMe");
+
+let randomNumber = Math.floor(Math.random() * 8);
+let playing = true;
 let eightBall = "";
 
-switch (randomNumber) {
-  case 0:
-    eightBall = "It is certain";
-    break;
-  case 1:
-    eightBall = "It is decidedly so";
-    break;
-  case 2:
-    eightBall = "Reply hazy, try again";
-    break;
-  case 3:
-    eightBall = "Cannot predict now";
-    break;
-  case 4:
-    eightBall = "Do not count on it";
-    break;
-  case 5:
-    eightBall = "My sources say no";
-    break;
-  case 6:
-    eightBall = "Outlook is not so good";
-    break;
-  case 7:
-    eightBall = "Signs point to yes";
-    break;
-}
+answers.classList.add("hidden");
+
+clickButton.addEventListener("click", function () {
+  answers.classList.remove("hidden");
+
+  switch (randomNumber) {
+    case 0:
+      eightBall = "It is certain";
+      break;
+    case 1:
+      eightBall = "It is decidedly so";
+      break;
+    case 2:
+      eightBall = "Reply hazy, try again";
+      break;
+    case 3:
+      eightBall = "Cannot predict now";
+      break;
+    case 4:
+      eightBall = "Do not count on it";
+      break;
+    case 5:
+      eightBall = "My sources say no";
+      break;
+    case 6:
+      eightBall = "Outlook is not so good";
+      break;
+    case 7:
+      eightBall = "Signs point to yes";
+    //break;
+  }
+});
 
 //console.log(`The Magic 8 Ball says ${eightBall}`);
 
